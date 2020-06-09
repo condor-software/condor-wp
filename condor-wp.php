f98f52e84cef9f89b773e53c879ea75b3289cdba
Diderick Oerlemans <diderick@oerlemans.nl>
2020-06-09 10:36:46 +0200

    vs project

--- condor-wp.php
+++ condor-wp.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: Condor Portal
- * Plugin URI:  http://www.condor.nl/developers/condor-portal-plugin
+ * Plugin Name: Condor WP plugin
+ * Plugin URI:  https://github.com/condor-software/condor-wp
  * Description: Condor portal functions
  * Version:     1.0
  * Author:      Condor B.V. 
@@ -14,43 +14,43 @@ if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-function condor_portal_menu() {
-	add_options_page('Condor Portal Options','Condor Portal','manage_options','condor_portal','condor_portal_options_page');
+function condor_menu() {
+	add_options_page('Condor Options','Condor','manage_options','condor','condor_options_page');
 }
-add_action('admin_menu', 'condor_portal_menu');
+add_action('admin_menu', 'condor_menu');
 
-function condor_portal_register() {
-	add_option('condor_portal_url', '');
-	register_setting('condor_portal_settings', 'condor_portal_url');
-	register_setting('condor_portal_settings', 'condor_portal_user');
-	register_setting('condor_portal_settings', 'condor_portal_pwd');
+function condor_register() {
+	add_option('condor_url', '');
+	register_setting('condor_settings', 'condor_url');
+	register_setting('condor_settings', 'condor_user');
+	register_setting('condor_settings', 'condor_pwd');
 }
-add_action('admin_init', 'condor_portal_register');
+add_action('admin_init', 'condor_register');
 
 ?>
 
 <?php
-function condor_portal_options_page() { 
+function condor_options_page() { 
 ?>
   <div class-"wrap">
   <h1>Condor Settings</h1>
   <form method="post" action="options.php">
-  <?php settings_fields( 'condor_portal_settings' ); ?>
-  <?php do_settings_sections( 'condor_portal_settings' ); ?>
+  <?php settings_fields( 'condor_settings' ); ?>
+  <?php do_settings_sections( 'condor_settings' ); ?>
 	<table class="form-table">
         <tr valign="top">
 	        <th scope="row">Condor URL</th>
-    	    <td><input type="text" name="condor_portal_url" value="<?php echo esc_attr( get_option('condor_portal_url') ); ?>" /></td>
+    	    <td><input type="text" name="condor_url" value="<?php echo esc_attr( get_option('condor_url') ); ?>" /></td>
         </tr>
          
         <tr valign="top">
         	<th scope="row">OData User</th>
-        	<td><input type="text" name="condor_portal_user" value="<?php echo esc_attr( get_option('condor_portal_user') ); ?>" /></td>
+        	<td><input type="text" name="condor_user" value="<?php echo esc_attr( get_option('condor_user') ); ?>" /></td>
         </tr>
         
         <tr valign="top">
         <th scope="row">OData Password</th>
-        <td><input type="text" name="condor_portal_pwd" value="<?php echo esc_attr( get_option('condor_portal_pwd') ); ?>" /></td>
+        <td><input type="text" name="condor_pwd" value="<?php echo esc_attr( get_option('condor_pwd') ); ?>" /></td>
         </tr>
     </table>  
   
